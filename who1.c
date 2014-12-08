f9387827b9250da5355cf4a822f2441e795cf9cf
acgtyrant <acgtyrant@gmail.com>
2014-12-08 18:57:55 +0800

    who 的第一个版本。相较于教材版，事实上我系统 utmp.h 里的 ut_time 是用预编译常量定义的。
    BUG:
    
    1. 时间直接用某单位计量，有待进一步格式化。
    2. 消除空白记录。
    
    Signed-off-by: acgtyrant <acgtyrant@gmail.com>

---
+++ who1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <utmp.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define SHOWHOST
+
+extern void show_info(struct utmp *utmp_record);
+
+int main(int argc, char *argv[]) {
+  int utmp_fd;
+  struct utmp utmp_record;
+  int utmp_size = sizeof(utmp_record);
+
+  if ((utmp_fd = open(UTMP_FILE, O_RDONLY)) == -1) {
+    perror(UTMP_FILE);
+    exit(EXIT_FAILURE);
+  }
+
+  while (read(utmp_fd, &utmp_record, utmp_size) == utmp_size) {
+    show_info(&utmp_record);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+extern void show_info(struct utmp *utmp_record) {
+  printf("%-8.8s", utmp_record->ut_user);
+  printf(" ");
+  printf("%-8.8s", utmp_record->ut_line);
+  printf(" ");
+  printf("%10ld", utmp_record->ut_time);
+  printf(" ");
+#ifdef SHOWHOST
+  printf("(%s)", utmp_record->ut_host);
+#endif
+  printf("\n");
+}
