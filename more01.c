4be49a6fdc49dc4b21b47b8cc98887a5406c5ef8
acgtyrant <acgtyrant@gmail.com>
2014-12-08 15:25:44 +0800

    more 的第一个版本，相对于教材版有代码排版上的进一步优化。
    BUG:
    
    1. 每次直接回车，均会重复输出 `more?`.
    2. 屏幕上滚时，`more?` 也会随之上滚。
    3. 按空格或 `q`, 还要按回车才生效。
    
    Signed-off-by: acgtyrant <acgtyrant@gmail.com>

---
+++ more01.c
@@ -0,0 +1,78 @@
+// more01.c - version of more
+// read and print 24 lines then pause for a few special commands
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PAGELEN 24
+#define LINELEN 512
+
+extern void do_more(FILE *fp);
+extern int see_more(void);
+
+int main(int argc, char *argv[]) {
+  FILE *fp;
+
+  if (argc == 1)
+    do_more(stdin);
+  else 
+    while (--argc) {
+      if ((fp = fopen(*++argv, "r")) != NULL) {
+        do_more(fp);
+        fclose(fp);
+      }
+      else {
+        printf("%s is an invalid file!\n", *argv);
+        exit(EXIT_FAILURE);
+      }
+    }
+  return EXIT_SUCCESS;
+}
+
+// 先打印行数：当行数达到 PAGELEN 时调用 see_more 函数处理命令；当输出到 stdout
+// 时发现 EOF, 自然终止。
+extern void do_more(FILE *fp) {
+  char line[LINELEN];
+  int num_of_lines = 0;
+  int reply;
+
+  while (fgets(line, LINELEN, fp)) {
+    if (num_of_lines == PAGELEN) {
+      reply = see_more();
+      if (reply == 0) {
+        exit(EXIT_SUCCESS);
+      }
+      num_of_lines -= reply;
+
+    }
+    if (fputs(line, stdout) == EOF) {
+      printf("No more lines.\n");
+      exit(EXIT_SUCCESS);
+    }
+    ++num_of_lines;
+  }
+}
+
+// 返回所要继续显示的行数。
+extern int see_more(void) {
+  int c;
+
+  printf("\033[7m more? \33[m\n");
+  //int bak = dup(1);
+  //int null = open("/dev/null", O_WRONLY);
+  //dup2(null, 1);
+  while ((c = getchar()) != EOF) {
+    switch(c) {
+      case 'q':
+        return 0;
+      case '\n':
+        return 1;
+      case ' ':
+        return PAGELEN;
+    }
+  }
+  //close(null);
+  //dup2(bak, 1);
+  //close(bak);
+  return 0;
+}
