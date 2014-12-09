891b0434fe47a0c5988148c8d2b127524a216bc6
acgtyrant <acgtyrant@gmail.com>
2014-12-09 20:32:31 +0800

    ls 的第一个版本。有待排序及格式化，此外不接受命令行参数，即所有参数按目录名来处理。
    Signed-off-by: acgtyrant <acgtyrant@gmail.com>

---
+++ 第三章/ls1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+extern void do_ls(char *dirname);
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    do_ls(".");
+  }
+  else {
+    while (--argc) {
+      printf("%s:\n", *++argv);
+      do_ls(*argv);
+    }
+  }
+  return EXIT_SUCCESS;
+}
+
+extern void do_ls(char *dirname) {
+  struct dirent *dirent_p;
+  DIR *dir_p = opendir(dirname);
+  if (dir_p == NULL) {
+    perror(dirname);
+    exit(EXIT_FAILURE);
+  }
+  while ((dirent_p = readdir(dir_p)) != NULL) {
+    printf("%s\n", dirent_p->d_name);
+  }
+  closedir(dir_p);
+  return ;
+}
