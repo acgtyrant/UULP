760fd5f40e5349cd39777efcf10f59f079b020ef
acgtyrant <acgtyrant@gmail.com>
2014-12-12 14:54:38 +0800

    P74 的 fileinfo.c
    教材版有点奇怪，main() 执行到尾部，竟然要返回非正常退出码即 1;也没有循环处理所有参数；也忽略了参数为目录的可能性，而是也直接把它当成文件来看。
    
    Signed-off-by: acgtyrant <acgtyrant@gmail.com>

---
+++ 第三章/fileinfo.c
@@ -0,0 +1,31 @@
+// fileinfo.c - use stat() to obtain and print file properties
+//            - some menbers are just numbers
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+extern void show_stat_info(char *filename, struct stat *stat_buf);
+
+int main(int argc, char *argv[]) {
+  struct stat stat_buf;
+  while (--argc > 0) {
+    if (stat(*++argv, &stat_buf) == -1) {
+      perror(*argv);
+      return EXIT_FAILURE;
+    }
+    show_stat_info(*argv, &stat_buf);
+  }
+  return EXIT_SUCCESS;
+}
+
+extern void show_stat_info(char *filename, struct stat *stat_buf) {
+  printf("  mode: %o\n", stat_buf->st_mode);
+  printf(" links: %d\n", stat_buf->st_nlink);
+  printf("  user: %d\n", stat_buf->st_uid);
+  printf(" group: %d\n", stat_buf->st_gid);
+  printf("  size: %d\n", stat_buf->st_size);
+  printf(" mtime: %d\n", stat_buf->st_mtime);
+  printf("  name: %s\n", filename);
+  return ;
+}
