0351f5cc4405980aaa94314bf402ce08459d98ce
acgtyrant <acgtyrant@gmail.com>
2014-12-12 16:14:15 +0800

    修正变量名，以符合指针地位。
    Signed-off-by: acgtyrant <acgtyrant@gmail.com>

--- 第三章/fileinfo.c
+++ 第三章/fileinfo.c
@@ -19,13 +19,13 @@ int main(int argc, char *argv[]) {
   return EXIT_SUCCESS;
 }
 
-extern void show_stat_info(char *filename, struct stat *stat_buf) {
-  printf("  mode: %o\n", stat_buf->st_mode);
-  printf(" links: %d\n", stat_buf->st_nlink);
-  printf("  user: %d\n", stat_buf->st_uid);
-  printf(" group: %d\n", stat_buf->st_gid);
-  printf("  size: %d\n", stat_buf->st_size);
-  printf(" mtime: %d\n", stat_buf->st_mtime);
+extern void show_stat_info(char *filename, struct stat *stat_buf_p) {
+  printf("  mode: %o\n", stat_buf_p->st_mode);
+  printf(" links: %d\n", stat_buf_p->st_nlink);
+  printf("  user: %d\n", stat_buf_p->st_uid);
+  printf(" group: %d\n", stat_buf_p->st_gid);
+  printf("  size: %d\n", stat_buf_p->st_size);
+  printf(" mtime: %d\n", stat_buf_p->st_mtime);
   printf("  name: %s\n", filename);
   return ;
 }
