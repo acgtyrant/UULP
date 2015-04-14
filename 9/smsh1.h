f2227ed771cf696f515b73d1d462b276089b525c
acgtyrant <acgtyrant@gmail.com>
2015-04-14 18:44:24 +0800

    9/smsh1.c: 加上注释以标明函数声明各属于什么源文件
    Signed-off-by: acgtyrant <acgtyrant@gmail.com>

--- 9/smsh1.h
+++ 9/smsh1.h
@@ -4,10 +4,13 @@
 #define YES 1
 #define NO  0
 
+// splitline.c
 extern char *next_cmd(char *prompt, FILE *fp);
 extern char **splitline(char *cmdline);
 extern void freelist(char **arglist);
 
+// execute.c
 extern int execute(char **);
 
+// smsh1.c
 extern void fatal(char *s1, char *s2, int n);
