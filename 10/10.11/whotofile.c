1604605194af60b2f28f9edf5f4f5cb1bff2d859
acgtyrant <acgtyrant@gmail.com>
2015-04-19 15:48:53 +0800

    完成编程练习 10.11
    Signed-off-by: acgtyrant <acgtyrant@gmail.com>

---
+++ 10/10.11/whotofile.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+int main(void) {
+  printf("About to run who into a file\n");
+
+  int pid = fork();
+  if (pid == -1) {
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid == 0) {
+    close(1);
+    open("userlist", O_CREAT | O_WRONLY | O_APPEND, 0644);
+    execlp("who", "who", NULL);
+  }
+
+  if (pid != 0) {
+    wait(NULL);
+    printf("Done running who. results in userlist\n");
+  }
+
+  return EXIT_SUCCESS;
+}
